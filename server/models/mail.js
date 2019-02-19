deb4176589c4d684a43397fc1f1afbff0222f3ba
Théo <theoporticcio@gmail.com>
2019-02-19 16:58:24 +0100

    Envoi du mail automatique, plus dans une méthode à appeler ...

--- server/models/mail.js
+++ server/models/mail.js
@@ -2,22 +2,20 @@
 
 module.exports = function(mail) {
     mail.observe('after save', function (ctx, next) {
-        mail.sendEmail = function(cb) {
-            mail.app.models.emailsender.send({
-              a: 'theoporticcio@gmail.com',
-              de: 'theorebdevtests@gmail.com',
-              sujet: 'my subject',
-              texte: 'my text',
-              html: 'my <em>html</em>'
-            }, function(err, mail) {
-              if (err) {
-                  console.log("Erreur envoi de mail " + err);
-              } else {
-                  console.log("Envoi réussi ! " + mail);
-              }
-            });
+        console.log("Entrée dans le hook");
+        mail.app.models.emailsender.send({
+          to: 'theoporticcio@gmail.com',
+          from: 'theorebdevtests@gmail.com',
+          subject: 'my subject',
+          text: 'my text', 
+          html: 'my <em>html</em>'
+        }, function(err, mail) {
+          if (err) {
+              console.log("Erreur envoi de mail " + err);
+          } else {
+              console.log("Envoi réussi ! " + mail);
           }
-
-        next()
+        });
+        next();
     });
 };
