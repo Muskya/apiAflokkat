48886155452b766de0f46b1f2c532d8c53375382
Théo <theoporticcio@gmail.com>
2019-02-20 13:51:08 +0100

    Small fixes + comprehension comments

--- server/models/emailsender.js
+++ server/models/emailsender.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//Pas de logique application en particulier, ce modèle
+//est simplement utilisé pour son héritage du built-in "Email"
+//et envoyer un mail depuis mail.js
 module.exports = function(Emailsender) {
 
 };
--- server/models/mail.js
+++ server/models/mail.js
@@ -1,13 +1,18 @@
 'use strict';
 
 module.exports = function(mail) {
+
+    //Hook après enregistrement d'un mail dans la BDD
     mail.observe('after save', function (ctx, next) {
         console.log("Entrée dans le hook");
         mail.app.models.emailsender.send({
-          to: 'theoporticcio@gmail.com',
-          from: 'theorebdevtests@gmail.com',
-          subject: 'my subject',
-          text: 'my text', 
+          to: 'theoporticcio@gmail.com', //Adresse élève (enregistré dans BDD)
+          from: 'theorebdevtests@gmail.com', //Adresse administration Aflokkat
+          subject: 'Questionnaire d\'évaluation à froid',
+          //Remplacer le texte par les nom/prénom de l'élève + lien vers 
+          //un nouveau questionnaire
+          text: 'Bonjour Nom_Eleve Prénom_Eleve, voici un lien vers le nouveau questionnaire' + 
+          'd\'évaluation à remplir :', 
           html: 'my <em>html</em>'
         }, function(err, mail) {
           if (err) {
@@ -18,4 +23,6 @@ module.exports = function(mail) {
         });
         next();
     });
+
+
 };
