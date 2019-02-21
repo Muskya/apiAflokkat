9282611962d8eba7f6d717dc1fe80731c82a2bc4
Théo <theoporticcio@gmail.com>
2019-02-21 13:57:42 +0100

    Dernier correctif

--- server/models/mail.js
+++ server/models/mail.js
@@ -4,9 +4,8 @@ module.exports = function(mail) {
 
     //Hook après enregistrement d'un mail dans la BDD
     mail.observe('after save', function (ctx, next) {
-      
+
         console.log("Entrée dans le hook");
-        console.log("Je vais enregistrer une instance de mail");
         console.log(ctx.instance); //Renvoi la structure du mail en JSON
         console.log(ctx.instance.a); //Renvoi l'adresse destinataire
         console.log(ctx.instance.de); //Renvoi l'adresse expéditrice
@@ -14,9 +13,9 @@ module.exports = function(mail) {
         mail.app.models.emailsender.send({
           to: ctx.instance.a, //Adresse élève (enregistré dans BDD)
           from: ctx.instance.de , //Adresse administration Aflokkat
-          subject: ctx.instance.sujet,
-          text: ctx.instance.texte, 
-          html: ctx.instance.html
+          subject: ctx.instance.sujet, //Sujet du mail 
+          text: ctx.instance.texte, //Texte du mail
+          html: ctx.instance.html //Html du mail
         }, function(err, mail) {
           if (err) {
               console.log("Erreur envoi de mail " + err);
@@ -26,6 +25,4 @@ module.exports = function(mail) {
         });
         next();
     });
-
-
 };
